06a6ec5bc87701ced2845a27d322633d2efdbd52
Don Williams <don.e.williams@gmail.com>
2026-07-26 21:24:29 -0400

    Added event driven disable of eDP-1 on lid close
     On branch development
     Your branch is up to date with 'origin/development'.
    
     Changes to be committed:
    	modified:   config/hypr/UserConfigs/Laptops.conf
    	modified:   config/hypr/UserConfigs/user_laptops.lua

--- config/hypr/UserConfigs/user_laptops.lua
+++ config/hypr/UserConfigs/user_laptops.lua
@@ -10,3 +10,13 @@
 -- Examples:
 -- hl.monitor({ output = "eDP-1", mode = "preferred", position = "auto", scale = "1" })
 -- hl.monitor({ output = "eDP-1", disabled = true })
+
+-- Lid close: remove laptop panel from layout
+hl.bind("switch:on:Lid Switch", function()
+  hl.monitor({ output = "eDP-1", disabled = true })
+end)
+
+-- Lid open: restore laptop panel
+hl.bind("switch:off:Lid Switch", function()
+  hl.monitor({ output = "eDP-1", disabled = false })
+end)
